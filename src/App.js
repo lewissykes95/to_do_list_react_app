e1c92540b4fa44d7d7985b1cd175a854b8ef6588
Lewis <lewisdsykes@gmail.com>
2022-08-01 18:51:17 +0100

    finished extensions

--- src/App.js
+++ src/App.js
@@ -17,10 +17,11 @@ const [newPriority, setNewPriority] = useState("")
 
 const toDoNodes = toDos.map((toDo, index) => {
   return(
-    <li key={index}>
+    <li key={index} className={toDo.priority}>
       <span>
-        Item: {toDo.name} | Priority: {toDo.priority}
+        Item: {toDo.name} 
       </span>
+  
     </li>
   );
 
@@ -33,8 +34,6 @@ const handleNewToDoInput = (event) => {
 const handlePriority = (event) => {
   setNewPriority(event.target.value);
 }
-
-
 
 const saveNewToDo = (event) => {
   event.preventDefault();
@@ -51,30 +50,40 @@ const saveNewToDo = (event) => {
   return (
     <div>
       <h1>To Do List:</h1>
-      <hr></hr>
+      
 
       <form onSubmit={saveNewToDo}>
-        <label htmlfor="add-toDo">Add to list:</label>
-        <input 
-        id="new-todo"
-        type="text"
-        value={newToDo}
-        onChange={handleNewToDoInput}/>
-        <input type="submit" value="save to list"/>
-        <label>High:</label>
-        <input
-        name="radios"
-        id="high-priority"
-        type="radio"
-        value="high"
-        onChange={handlePriority}/>
-        <label>Low:</label>
-        <input 
-        name="radios"
-        id="low-priority"
-        type="radio"
-        value="low"
-        onChange={handlePriority}/>
+        <div class="flex-item">
+          <label htmlfor="add-toDo">Add to list:</label>
+          <input 
+          id="new-todo"
+          type="text"
+          value={newToDo}
+          onChange={handleNewToDoInput}/>
+        </div>
+
+        <div class="flex-item">
+          <input type="submit" value="save to list" class="button"/>
+        </div>
+
+        <div class="flex-item">
+          <label>High:</label>
+          <input
+          name="radios"
+          id="high-priority"
+          type="radio"
+          value="high"
+          onChange={handlePriority}/>
+
+          <label>Low:</label>
+          <input 
+          name="radios"
+          id="low-priority"
+          type="radio"
+          value="low"
+          onChange={handlePriority}/>
+        </div>
+       
       </form>
 
       <ul>
